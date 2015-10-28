ef8db132cb8dba88b77527624f01ac01b86c2186
mchivuku <mchivuku@iu.edu>
2015-10-28 16:08:01 -0400

    Updated the emergency information based on Tamir's request

--- app/views/includes/emergencyinfo.blade.php
+++ app/views/includes/emergencyinfo.blade.php
@@ -1,10 +1,12 @@
      <div class="emergency-info">
                     <h2>For Emergencies, <span>Call 911</span></h2>
-                    <p>If this is not an emergency, call (555) 123-9876 if you:</p>
+                    <p>If this is not an emergency, call (812) 855-7688 if you:</p>
                     <ul>
                         <li>Need an appointment in the next hour</li>
                         <li>Need to schedule multiple appointments</li>
                         <li>Are enrolled in the intensive english program</li>
-                        <li>Are under 18 years of age</li> <li>Please note: The IU Health Center is not a Medicare or HIP provider</li>
+                        <li>Are under 18 years of age</li>
+                        <li>Please note: The IU Health Center is not a Medicare or HIP provider</li>
+                        <li>If you don’t see a specific appointment reason, please call  our appointment line at (812)-855-7688</li>
                     </ul>
                 </div>
